657c9318acb354fec96295f58167ca98a7ba229a
ромка <romawere.h@gmail.com>
2025-08-11 18:31:35 +0300

    release

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+TOKEN_TG = os.environ.get("TOKEN_TG")
+TOKEN_WEATHER = os.environ.get("TOKEN_WEATHER")
+BASE_URL_NOW = "https://api.openweathermap.org/data/2.5/weather"
+BASE_URL_FORECAST = "https://api.openweathermap.org/data/2.5/forecast"
